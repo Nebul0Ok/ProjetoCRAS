01c5ef84c17eb5fd974bdc24279cba0cfc00ab3d
Nebulook <joaogustavofatec@gmail.com>
2026-04-20 22:26:08 -0300

    Novo commit

---
+++ AbasteceCRAS/MVVM/Views/TelaLogin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AbasteceCRAS.MVVM.Models;
+using AbasteceCRAS.MVVM.ViewModels;
+using System.Windows;
+
+namespace AbasteceCRAS.MVVM.Views;
+
+public partial class TelaLogin : Window
+{
+    public static List<Usuario> UsuariosCadastrados;
+    public TelaLogin()
+    {
+        InitializeComponent();
+        this.DataContext = new LoginViewModel();
+        UsuariosCadastrados = new List<Usuario>();
+        Usuario u = new Usuario("Carlos", "carlos@email.com", "carlos123");
+        Usuario admin = new Usuario("Admin", "admin", "admin");
+        UsuariosCadastrados.Add(u);
+        UsuariosCadastrados.Add(admin);
+    }
+
+    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is LoginViewModel vm)
+        {
+            var passwordBox = sender as System.Windows.Controls.PasswordBox;
+            vm.Senha = passwordBox.Password;
+        }
+    }
+}
