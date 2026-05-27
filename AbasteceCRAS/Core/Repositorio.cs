cd3d9764276141b90adfe7aaa6e9563e06d9a8ab
Nebul0Ok <joaogustavofatec@gmail.com>
2026-05-26 21:39:18 -0300

    Criacao do Repositorio na Memoria

---
+++ AbasteceCRAS/Core/Repositorio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AbasteceCRAS.Core;
+
+public class Repositorio<T>
+{
+    public ObservableCollection<T> Lista { get; set; }
+    public Dictionary<int, T> Dicionario { get; set; }
+    private int ProximoItemLista = 1;
+
+    public Repositorio()
+    {
+        Lista = new ObservableCollection<T>();
+        Dicionario = new Dictionary<int, T>();
+    }
+
+    public void AdicionarItem(T item)
+    {
+        if (Dicionario.ContainsValue(item)) return;
+
+        Dicionario.Add(ProximoItemLista, item);
+        Lista.Add(item);
+        ProximoItemLista++;
+    }
+
+}
